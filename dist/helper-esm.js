096c0fa82f0bbc86b64a03c99eb5bb21a08c5b6c
Abdul Habra <abdul.habra@gmail.com>
2021-02-21 18:41:24 -0500

    upgrade build version

--- dist/helper-esm.js
+++ dist/helper-esm.js
@@ -1,6 +1,6 @@
 // Halper.js Common ES/JS utility library
 // https://github.com/ahabra/js-helper
-// Copyright 2021 (C) Abdul Habra. Version 0.1.0.
+// Copyright 2021 (C) Abdul Habra. Version 0.1.1.
 // Apache License Version 2.0
 
 
