fc2bde528002bc76fb6c70564716005657f3a287
Abdul Habra <abdul.habra@gmail.com>
2022-11-08 22:27:02 -0500

    created dist

--- dist/helper-script.js
+++ dist/helper-script.js
@@ -1,6 +1,6 @@
 // Halper.js Common ES/JS utility library
 // https://github.com/ahabra/js-helper
-// Copyright 2022 (C) Abdul Habra. Version 0.5.1.
+// Copyright 2022 (C) Abdul Habra. Version 0.6.0.
 // Apache License Version 2.0
 
 
