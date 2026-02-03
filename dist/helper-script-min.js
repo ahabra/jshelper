b0398660b2cec9d0bc105ff2281ce38fe80f4b20
Abdul Habra <abdul.habra@gmail.com>
2026-02-03 17:49:17 -0500

    upgrade dependencies

--- dist/helper-script-min.js
+++ dist/helper-script-min.js
@@ -1,6 +1,6 @@
 // Halper.js Common ES/JS utility library
 // https://github.com/ahabra/js-helper
-// Copyright 2022 (C) Abdul Habra. Version 0.7.0.
+// Copyright 2022 (C) Abdul Habra. Version 0.7.3.
 // Apache License Version 2.0
 
 
